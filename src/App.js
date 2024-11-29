b17b56e9962857742dccf210b3438e9631734590
AnkitaMondal07 <ankitakartikmondal@gmail.com>
2024-11-29 20:25:53 +0530

    feat: create react project and first test case pas

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './pages/StringCalculator';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <StringCalculator />
+    </div>
+  );
+}
+
+export default App;
